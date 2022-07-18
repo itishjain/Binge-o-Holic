92aeadda608297883739983ffec110d16932b014
itishjain <itish.jain2301@gmail.com>
2022-07-18 11:20:46 +0530

    Changes commited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is my home page
+      </Route>
+      <Route exact path="/starred">
+        This is starred page
+      </Route>
+      <Route>
+        <h1 align="center">*************** Error 404 *************** </h1>
+        <h1 align="center">An unkown error occured.</h1>
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
