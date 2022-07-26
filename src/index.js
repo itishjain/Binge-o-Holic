68c80ad33f9b3accec88aa2e1cbcf3731f9de43d
itishjain <itish.jain2301@gmail.com>
2022-07-26 12:31:35 +0530

    resize the search bar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import ServiceWorker from './service-worker';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
@@ -17,7 +16,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 ServiceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
