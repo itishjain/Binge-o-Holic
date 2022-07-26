dcd45686f021f67708f2c798ed0f28e7f69438a5
itishjain <itish.jain2301@gmail.com>
2022-07-26 12:06:55 +0530

    Icons and service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import ServiceWorker from './service-worker';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
@@ -16,7 +17,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 ServiceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
